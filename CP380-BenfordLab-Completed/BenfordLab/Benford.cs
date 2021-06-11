8405304a3f836506ae0c1a7e2bf529df3f5b2772
Nilkumar Patel <nsp4898@gmail.com>
2021-06-10 21:04:09 -0400

    CP380 Lab Benford Completed

---
+++ CP380-BenfordLab-Completed/BenfordLab/Benford.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace BenfordLab
+{
+    public class BenfordData
+    {
+        public int Digit { get; set; }
+        public int Count { get; set; }
+
+        public BenfordData() { }
+    }
+
+    public class Benford
+    {
+       
+        public static BenfordData[] calculateBenford(string csvFilePath)
+        {
+            // load the data
+            var data = File.ReadAllLines(csvFilePath)
+                .Skip(1) // For header
+                .Select(s => Regex.Match(s, @"^(.*?),(.*?)$"))
+                .Select(data => new
+                {
+                    Country = data.Groups[1].Value,
+                    Population = int.Parse(data.Groups[2].Value)
+                });
+
+            // manipulate the data!
+            //
+            // Select() with:
+            //   - Country
+            //   - Digit (using: FirstDigit.getFirstDigit() )
+            // 
+            // Then:
+            //   - you need to count how many of *each digit* there are
+            //
+            // Lastly:
+            //   - transform (select) the data so that you have a list of
+            //     BenfordData objects
+            //
+
+            var m = data.GroupBy(
+                    row => FirstDigit.getFirstDigit(row.Population)
+                ).Select(
+                    groups => new BenfordData
+                    {
+                        Digit = groups.Key,
+                        Count = groups.Count()
+                    }
+                );
+
+            return m.ToArray();
+        }
+    }
+}
---
+++ CP380-BenfordLab-Completed/BenfordLab/FirstDigit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordLab
+{
+    public class FirstDigit
+    {
+        /* Given an integer X, this method returns the first digit
+         * 
+         * For example,
+         *   5 ==> returns 5
+         *   10 ==> returns 1
+         *   654321 ==> returns 6
+         */
+        public static int getFirstDigit(int digit) {
+            // TODO
+            // getting the divisor using the total digits in the number
+            int divisor = (int)Math.Pow(10, digit.ToString().Length - 1);
+            return digit / divisor;
+        }
+    }
+}
